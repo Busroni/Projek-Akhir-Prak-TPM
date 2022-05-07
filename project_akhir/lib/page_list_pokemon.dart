f15029c2aa29a8a877b15ebee7e5b85349e21903
Busroni <busroni10@gmail.com>
2022-05-07 22:40:51 +0700

    update pokemon list

--- project_akhir/lib/page_list_pokemon.dart
+++ project_akhir/lib/page_list_pokemon.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'model/pokemon_list_model.dart';
 import 'pokemon_data_source.dart';
@@ -56,21 +58,38 @@ class _PageListPokemonState extends State<PageListPokemon> {
   }
 
   Widget _buildSuccessSection(PokemonListModel data) {
-    return ListView.builder(
-      itemCount: data.results?.length,
-      itemBuilder: (BuildContext context, int index) {
-        return InkWell(
-          onTap: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return DetailPokemon(
-                pokemonName: "${data.results?[index].name}",
-              );
-            }));
-          },
-          child: _buildItemPokemonList("${data.results?[index].name}"),
-        );
-        // return _buildItemCountries("${data.results?[index].name}");
-      },
+    return SafeArea(
+      child: Column(
+        children: [
+          Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+          Text("Pokemon",
+            style: TextStyle(fontSize: 30,
+              fontWeight: FontWeight.bold,
+            color: Colors.blue,letterSpacing: 10),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 20),
+            height: 650,
+            child: ListView.builder(
+            itemCount: data.results?.length,
+              itemBuilder: (BuildContext context, int index) {
+                return InkWell(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) {
+                      return DetailPokemon(
+                        pokemonName: "${data.results?[index].name}",
+                      );
+                    }));
+                  },
+                  child: _buildItemPokemonList("${data.results?[index].name}"),
+                );
+                // return _buildItemCountries("${data.results?[index].name}");
+              },
+            ),
+          ),
+        ],
+      )
+
     );
   }
 
@@ -80,6 +99,7 @@ class _PageListPokemonState extends State<PageListPokemon> {
         padding: const EdgeInsets.all(15.0),
         child: Row(
           children: [
+
             Text(
               '${name}',
               style: TextStyle(fontSize: 20),
--- project_akhir/test/widget_test.dart
+++ project_akhir/test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:loginregisterlocaldata/main.dart';
 import 'package:loginregisterlocaldata/xmain.dart';
 
 void main() {
